ea6bddabee3e0ae0658e9db7565321372a7fc11c
zack <moser.zh25@gmail.com>
2025-05-07 19:56:47 -0700

    Depth texture

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use winit::window::Window;
 use winit::{
     event::*,
     event_loop::EventLoop,
-    keyboard::{KeyCode, PhysicalKey},
+    keyboard::KeyCode,
     window::WindowBuilder,
 };
 
@@ -127,6 +127,7 @@ struct State<'a> {
     camera_buffer: wgpu::Buffer,
     camera_bind_group: wgpu::BindGroup,
     camera_rotation_radians: f32,
+    depth_texture: texture::Texture
 }
 
 impl<'a> State<'a> {
@@ -289,6 +290,8 @@ impl<'a> State<'a> {
             label: Some("camera_bind_group"),
         });
 
+        let depth_texture = texture::Texture::create_depth_texture(&device, &config, "depth_texture");
+
         let shader = device.create_shader_module(wgpu::include_wgsl!("shader.wgsl"));
 
         let render_pipeline_layout =
@@ -332,7 +335,13 @@ impl<'a> State<'a> {
                 // Requires Features::CONSERVATIVE_RASTERIZATION
                 conservative: false,
             },
-            depth_stencil: None,
+            depth_stencil: Some(wgpu::DepthStencilState {
+                format: texture::Texture::DEPTH_FORMAT,
+                depth_write_enabled: true,
+                depth_compare: wgpu::CompareFunction::Less,
+                stencil: wgpu::StencilState::default(),
+                bias: wgpu::DepthBiasState::default()
+            }),
             multisample: wgpu::MultisampleState {
                 count: 1,
                 mask: !0,
@@ -384,6 +393,7 @@ impl<'a> State<'a> {
             camera_buffer,
             camera_bind_group,
             camera_rotation_radians,
+            depth_texture
         }
     }
 
@@ -397,6 +407,8 @@ impl<'a> State<'a> {
             self.config.width = new_size.width;
             self.config.height = new_size.height;
             self.surface.configure(&self.device, &self.config);
+
+            self.depth_texture = texture::Texture::create_depth_texture(&self.device, &self.config, "depth_texture");
         }
     }
 
@@ -433,7 +445,14 @@ impl<'a> State<'a> {
                         store: wgpu::StoreOp::Store,
                     },
                 })],
-                depth_stencil_attachment: None,
+                depth_stencil_attachment: Some(wgpu::RenderPassDepthStencilAttachment {
+                    view: &self.depth_texture.view,
+                    depth_ops: Some(wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(1.0),
+                        store: wgpu::StoreOp::Store
+                    }),
+                    stencil_ops: None,
+                }),
                 occlusion_query_set: None,
                 timestamp_writes: None,
             });
@@ -464,6 +483,9 @@ fn handle_window_event(
         WindowEvent::KeyboardInput { event, .. } => handle_key_input(event, control_flow, state),
         WindowEvent::Resized(physical_size) => state.resize(*physical_size),
         WindowEvent::RedrawRequested => {
+            if !state.window.is_visible().unwrap() {
+                state.window.set_visible(true);
+            }
             // This tells winit that we want another frame after this one
             state.window().request_redraw();
 
@@ -498,9 +520,9 @@ fn handle_key_input(
     state: &mut State
 ) {
     let mut update_camera = |angle| {
-        let x = f32::sin(angle);
+        let x = f32::sin(angle) * 5.;
         let y = state.camera.eye.y;
-        let z = f32::cos(angle);
+        let z = f32::cos(angle) * 5.;
         state.camera.eye = cgmath::point3(x, y, z);
 
         let mut uniform = CameraUniform::new();
@@ -535,7 +557,10 @@ pub async fn run() {
     }
 
     let event_loop = EventLoop::new().unwrap();
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
+    let window = WindowBuilder::new()
+    .with_title("WGPU")
+    .with_visible(false)
+    .build(&event_loop).unwrap();
 
     #[cfg(target_arch = "wasm32")]
     {
