d6471581578eece4ae7fae8dd1db49d519967b42
zack <moser.zh25@gmail.com>
2025-05-26 13:21:22 -0700

    Create Mesh type

--- src/geometry.rs
+++ src/geometry.rs
@@ -1,3 +1,8 @@
+use std::{fs::File, io::BufReader, path::Path};
+use wgpu::util::DeviceExt;
+
+type MeshIndex = u16;
+
 #[repr(C)]
 #[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
 pub struct Vertex {
@@ -23,5 +28,76 @@ impl Vertex {
                 },
             ],
         }
+    }
+}
+
+pub struct Mesh {
+    vertices: Vec<Vertex>,
+    indices: Vec<MeshIndex>,
+    vertex_buffer: wgpu::Buffer,
+    index_buffer: wgpu::Buffer
+}
+
+impl Mesh {
+    pub fn new(device: &wgpu::Device, label: Option<&str>) -> Self {
+        let vertices = Vec::new();
+        let indices = Vec::new();
+        let vertex_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+            label,
+            size: 0,
+            usage: wgpu::BufferUsages::VERTEX,
+            mapped_at_creation: false
+        });
+        let index_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+            label,
+            size: 0,
+            usage: wgpu::BufferUsages::INDEX,
+            mapped_at_creation: false
+        });
+
+        Self {
+            vertices,
+            indices,
+            vertex_buffer,
+            index_buffer
+        }
+    }
+
+    pub fn from_obj(device: &wgpu::Device, obj: obj::Obj<obj::TexturedVertex>)-> anyhow::Result<Self> {
+        let vertices: Vec<Vertex> = obj.vertices.iter().map(|v: &obj::TexturedVertex| {
+            Vertex { position: v.position, tex_coords: v.texture[..2].try_into().unwrap() }
+        }).collect();
+        let indices: Vec<u16> = obj.indices;
+
+        let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Vertex Buffer"),
+            contents: bytemuck::cast_slice(&vertices),
+            usage: wgpu::BufferUsages::VERTEX,
+        });
+
+        let index_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Index Buffer"),
+            contents: bytemuck::cast_slice(&indices),
+            usage: wgpu::BufferUsages::INDEX,
+        });
+
+        Ok(Self {
+            vertices,
+            indices,
+            vertex_buffer,
+            index_buffer
+        })
+    }
+
+    pub fn from_obj_bytes(device: &wgpu::Device, obj_bytes: &[u8]) -> anyhow::Result<Self> {
+        let obj: obj::Obj<obj::TexturedVertex> = obj::load_obj(obj_bytes)?;
+        Self::from_obj(device, obj)
+    }
+
+    pub fn from_obj_path<P: AsRef<Path>>(device: &wgpu::Device, obj_path: P) -> anyhow::Result<Self> {
+        let obj_file = File::open(obj_path)?;
+        let obj_reader = BufReader::new(obj_file);
+        let obj: obj::Obj<obj::TexturedVertex> = obj::load_obj(obj_reader)?;
+        Self::from_obj(device, obj)
     }
 }
