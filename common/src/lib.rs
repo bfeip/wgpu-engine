71cb347a5ebf4fbddaa7d9559d00ed9016aaa256
zack <moser.zh25@gmail.com>
2026-03-02 13:06:04 -0800

    More color constants

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -37,6 +37,11 @@ impl RgbaColor {
     pub const GREEN: Self = Self { r: 0.0, g: 1.0, b: 0.0, a: 1.0 };
     pub const BLUE: Self = Self { r: 0.0, g: 0.0, b: 1.0, a: 1.0 };
     pub const MAGENTA: Self = Self { r: 1.0, g: 0.0, b: 1.0, a: 1.0 };
+    pub const CYAN: Self = Self { r: 0.0, g: 1.0, b: 1.0, a: 1.0 };
+    pub const YELLOW: Self = Self { r: 1.0, g: 1.0, b: 0.0, a: 1.0 };
+    pub const ORANGE: Self = Self { r: 1.0, g: 0.5, b: 0.0, a: 1.0 };
+    pub const GRAY: Self = Self { r: 0.5, g: 0.5, b: 0.5, a: 1.0 };
+    pub const TRANSPARENT: Self = Self { r: 0.0, g: 0.0, b: 0.0, a: 0.0 };
 }
 
 /// Computes the normal matrix from a world transform matrix.
