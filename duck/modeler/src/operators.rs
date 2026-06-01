084bf3fc05f35cd349fb1f0ac097c817a8239473
Zachary Harvie <moser.zh25@gmail.com>
2026-06-01 08:16:12 -0700

    Black CAD lines

--- duck/modeler/src/operators.rs
+++ duck/modeler/src/operators.rs
@@ -21,7 +21,7 @@ impl ConstructionOptions {
             tessellation_tolerance: 0.01,
             scale_factor: 1.0,
             face_color: RgbaColor { r: 0.55, g: 0.65, b: 0.9, a: 1.0 },
-            edge_color: RgbaColor { r: 0.2, g: 0.2, b: 0.5, a: 1.0 },
+            edge_color: RgbaColor { r: 0.0, g: 0.0, b: 0.0, a: 1.0 },
             include_edges: true,
         };
         let construction_plane = Plane::xz();
